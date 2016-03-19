0095cd290e25d42602a44740f53649c95fd7367f
LynAs <szlynas@gmail.com>
2016-03-19 21:18:11 +0600

    clean up

--- src/main/java/com/brahalla/Cerberus/controller/AuthenticationController.java
+++ src/main/java/com/brahalla/Cerberus/controller/AuthenticationController.java
@@ -27,51 +27,51 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("${cerberus.route.authentication}")
 public class AuthenticationController {
 
-  private final Logger logger = Logger.getLogger(this.getClass());
+    private final Logger logger = Logger.getLogger(this.getClass());
 
-  @Value("${cerberus.token.header}")
-  private String tokenHeader;
+    @Value("${cerberus.token.header}")
+    private String tokenHeader;
 
-  @Autowired
-  private AuthenticationManager authenticationManager;
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
-  @Autowired
-  private TokenUtils tokenUtils;
+    @Autowired
+    private TokenUtils tokenUtils;
 
-  @Autowired
-  private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
-  @RequestMapping(method = RequestMethod.POST)
-  public ResponseEntity<?> authenticationRequest(@RequestBody AuthenticationRequest authenticationRequest, Device device) throws AuthenticationException {
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<?> authenticationRequest(@RequestBody AuthenticationRequest authenticationRequest, Device device) throws AuthenticationException {
 
-    // Perform the authentication
-    Authentication authentication = this.authenticationManager.authenticate(
-      new UsernamePasswordAuthenticationToken(
-        authenticationRequest.getUsername(),
-        authenticationRequest.getPassword()
-      )
-    );
-    SecurityContextHolder.getContext().setAuthentication(authentication);
+        // Perform the authentication
+        Authentication authentication = this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-    // Reload password post-authentication so we can generate token
-    UserDetails userDetails = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-    String token = this.tokenUtils.generateToken(userDetails, device);
+        // Reload password post-authentication so we can generate token
+        UserDetails userDetails = this.userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
+        String token = this.tokenUtils.generateToken(userDetails, device);
 
-    // Return the token
-    return ResponseEntity.ok(new AuthenticationResponse(token));
-  }
-
-  @RequestMapping(value = "${cerberus.route.authentication.refresh}", method = RequestMethod.GET)
-  public ResponseEntity<?> authenticationRequest(HttpServletRequest request) {
-    String token = request.getHeader(this.tokenHeader);
-    String username = this.tokenUtils.getUsernameFromToken(token);
-    CerberusUser user = (CerberusUser) this.userDetailsService.loadUserByUsername(username);
-    if (this.tokenUtils.canTokenBeRefreshed(token, user.getLastPasswordReset())) {
-      String refreshedToken = this.tokenUtils.refreshToken(token);
-      return ResponseEntity.ok(new AuthenticationResponse(refreshedToken));
-    } else {
-      return ResponseEntity.badRequest().body(null);
+        // Return the token
+        return ResponseEntity.ok(new AuthenticationResponse(token));
     }
-  }
+
+    @RequestMapping(value = "${cerberus.route.authentication.refresh}", method = RequestMethod.GET)
+    public ResponseEntity<?> authenticationRequest(HttpServletRequest request) {
+        String token = request.getHeader(this.tokenHeader);
+        String username = this.tokenUtils.getUsernameFromToken(token);
+        CerberusUser user = (CerberusUser) this.userDetailsService.loadUserByUsername(username);
+        if (this.tokenUtils.canTokenBeRefreshed(token, user.getLastPasswordReset())) {
+            String refreshedToken = this.tokenUtils.refreshToken(token);
+            return ResponseEntity.ok(new AuthenticationResponse(refreshedToken));
+        } else {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
 
 }
